d5fd539be580544cb9af70a17c6ff69dca37bad3
tuguzT <timurka.tugushev@gmail.com>
2024-10-12 21:30:14 +0300

    Control balls with combined input of all players (aka 'gravity' from players pulling one game board on each other's side)

--- Assets/BiaM/Scripts/Ball.cs
+++ Assets/BiaM/Scripts/Ball.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Mirror;
+using ProceduralToolkit;
 using Terresquall;
 using UnityEngine;
 
@@ -7,7 +9,13 @@ namespace BiaM
     [RequireComponent(typeof(PredictedRigidbody))]
     public class Ball : NetworkBehaviour
     {
-        [SerializeField, Min(0f)] private float forceMultiplier = 1f;
+        [SerializeField, Header("General"), Min(0f)]
+        private float forceMultiplier = 1f;
+
+        [SerializeField, Header("Network"), ReadOnly, SyncVar]
+        private Vector2 combinedNetworkInputs;
+
+        private readonly SyncDictionary<int, Vector2> _networkInputs = new();
 
         private PredictedRigidbody _predictedRigidbody;
 
@@ -16,15 +24,38 @@ namespace BiaM
             _predictedRigidbody = GetComponent<PredictedRigidbody>();
         }
 
+        private void Update()
+        {
+            if (isClient && VirtualJoystick.GetAxisDelta() != Vector2.zero)
+            {
+                CmdUpdateInput(VirtualJoystick.GetAxis());
+            }
+
+            if (!isServer) return;
+
+            var result = _networkInputs.Values.Aggregate(Vector2.zero, (curr, item) => curr + item);
+            result.x = Mathf.Clamp(result.x, -1f, 1f);
+            result.y = Mathf.Clamp(result.y, -1f, 1f);
+            combinedNetworkInputs = result;
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdUpdateInput(Vector2 input)
+        {
+            var key = NetworkClient.connection.connectionId;
+            _networkInputs[key] = input;
+        }
+
         private void FixedUpdate()
         {
-            var input = VirtualJoystick.GetAxis();
-            var force = new Vector3(input.x, input.y, 1f) * forceMultiplier;
+            var force = (combinedNetworkInputs.ToVector3XY() + Vector3.forward) * forceMultiplier;
             _predictedRigidbody.predictedRigidbody.AddForce(force);
         }
 
         private void OnTriggerEnter(Collider other)
         {
+            if (!isServer) return;
+
             Debug.Log($"{gameObject.tag} entered trigger {other.gameObject.tag}");
         }
     }
