d4a250a5ac9157293d6185965e1e7c58ea2a379a
tuguzT <timurka.tugushev@gmail.com>
2024-10-13 19:10:23 +0300

    Render player name above its ball

--- Assets/BiaM/Scripts/Ball.cs
+++ Assets/BiaM/Scripts/Ball.cs
@@ -1,5 +1,6 @@
 using Mirror;
 using ProceduralToolkit;
+using TMPro;
 using UnityEngine;
 
 namespace BiaM
@@ -10,20 +11,37 @@ namespace BiaM
         [SerializeField, Header("General"), Min(0f)]
         private float forceMultiplier = 1f;
 
+        [SerializeField] private TMP_Text textPrefab;
+
         private PredictedRigidbody _predictedRigidbody;
         private InputManager _inputManager;
         private Material _material;
+        private TMP_Text _text;
+        private RectTransform _textRectTransform;
 
         private void Awake()
         {
             _predictedRigidbody = GetComponent<PredictedRigidbody>();
             _inputManager = FindObjectOfType<InputManager>();
             _material = GetComponentInChildren<Renderer>().material;
+
+            var mainCanvas = GameObject.FindWithTag("MainCanvas").GetComponent<Canvas>();
+            _text = Instantiate(textPrefab, mainCanvas.transform);
+            _textRectTransform = _text.GetComponent<RectTransform>();
         }
 
         private void Start()
         {
             _material.color = Color.red;
+
+            _text.text = gameObject.name;
+            _text.color = Color.red;
+        }
+
+        private void Update()
+        {
+            var worldPoint = _predictedRigidbody.transform.TransformPoint(Vector3.zero) + Vector3.up * 2f;
+            _textRectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPoint);
         }
 
         private void FixedUpdate()
@@ -38,6 +56,11 @@ namespace BiaM
             if (!isServer) return;
 
             Debug.Log($"{gameObject.tag} entered trigger {other.gameObject.tag}");
+        }
+
+        private void OnDestroy()
+        {
+            Destroy(_text);
         }
     }
 }
