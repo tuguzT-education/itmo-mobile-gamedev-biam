67c2e907682707293b4fb0c3cb75f7deedc7eed8
tuguzT <timurka.tugushev@gmail.com>
2024-10-15 15:51:24 +0300

    Enable automatic LAN network discovery to just press `Play` button and, well, play a game

---
+++ Assets/BiaM/Scripts/MainMenuManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using Mirror.Discovery;
+using TMPro;
+using UnityEngine;
+
+namespace BiaM
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private NetworkDiscovery networkDiscovery;
+        [SerializeField] private TMP_Text infoText;
+
+        private readonly Dictionary<long, ServerResponse> _discoveredServers = new();
+
+        public void Play()
+        {
+            infoText.text = "Starting game...";
+            StartCoroutine(Waiter());
+        }
+
+        public void OnDiscoveredServer(ServerResponse info)
+        {
+            _discoveredServers[info.serverId] = info;
+            Connect(info);
+        }
+
+        public void Quit()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+
+        private void Connect(ServerResponse info)
+        {
+            infoText.text = $"Connecting to {info.serverId}...";
+            networkDiscovery.StopDiscovery();
+            NetworkManager.singleton.StartClient(info.uri);
+        }
+
+        private IEnumerator Waiter()
+        {
+            infoText.text = "Discovering servers...";
+            _discoveredServers.Clear();
+
+            networkDiscovery.StartDiscovery();
+            yield return new WaitForSeconds(3.5f);
+
+            if (_discoveredServers is { Count: > 0 }) yield break;
+            infoText.text = "No server found, starting a new game...";
+            yield return new WaitForSeconds(1.0f);
+
+            _discoveredServers.Clear();
+            NetworkManager.singleton.StartHost();
+            networkDiscovery.AdvertiseServer();
+        }
+    }
+}
