99d83140eafcff8b3780e870960feec1558b6768
tuguzT <timurka.tugushev@gmail.com>
2024-10-17 13:21:13 +0300

    Import & integrate Firebase Crashlytics

---
+++ Assets/BiaM/Scripts/FirebaseManager.cs
@@ -0,0 +1,36 @@
+﻿using Firebase;
+using Firebase.Crashlytics;
+using Firebase.Extensions;
+using UnityEngine;
+
+namespace BiaM
+{
+    public class FirebaseManager : MonoBehaviour
+    {
+        [SerializeField] private TextAsset jsonConfig;
+
+        private FirebaseApp _app;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+            {
+                var dependencyStatus = task.Result;
+                if (dependencyStatus != DependencyStatus.Available)
+                {
+                    Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+                    return;
+                }
+
+                _app = Application.isEditor
+                    ? FirebaseApp.Create(AppOptions.LoadFromJsonConfig(jsonConfig.text), "ball-in-a-maze-editor")
+                    : FirebaseApp.DefaultInstance;
+                Debug.Log("Firebase initialized successfully");
+
+                Crashlytics.ReportUncaughtExceptionsAsFatal = true;
+            });
+        }
+    }
+}
